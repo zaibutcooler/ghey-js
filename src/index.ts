e5203f718bdbdff5e6b985f973f198573810dcd5
Zai <130903099+zaibutcooler@users.noreply.github.com>
2024-04-27 17:58:10 +0630

    see how deployment goes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
+import { something } from './utils';
+
 export const dummyDemo = () => {
   return 'Why are you ghey';
+};
+
+export const plus = (x: number, y: number): number => {
+  return x + y;
+};
+
+export const minus = (x: number, y: number): number => {
+  return x - y;
+};
+
+export const multiply = (x: number, y: number): number => {
+  return x * y;
+};
+
+export const divide = (x: number, y: number): number => {
+  return x / y;
+};
+
+export const logSomething = () => {
+  console.log(something);
 };
---
+++ src/utils.ts
@@ -0,0 +1,3 @@
+export const something = () => {
+  return 'something';
+};
